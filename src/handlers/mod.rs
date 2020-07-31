98628d1a3142b77f9e17f4ff193e7026104fe3cb
Aaron Marshall <hi@imaaronnicetomeetyou.me>
2020-08-01 01:12:42 +1000

    Adds documentation and fixes some formatting issues

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -6,8 +6,29 @@ use rusoto_s3::util::{PreSignedRequest, PreSignedRequestOption};
 use std::convert::Infallible;
 use std::time::Duration;
 
+/// The expiration of our pre-signed urls in minutes.
+/// This is used in conjunction with Duration::from_secs(u64).
 const URL_EXPIRATION_MINUTES: u64 = 5;
 
+/// Creates a signed URL from the given `bucket_name`, `region`, and `credentials`.
+/// Returns a `Result` of `warp::Reply`.
+///
+/// # Examples
+///
+/// ```
+/// let region = "ap-southeast-2".parse().expect("Region must be valid.");
+/// let creds = StaticProvider::new_minimial("test", "test").credentials().await.unwrap();
+/// let filter = warp::path!("/my/path")
+/// 	.and(warp::post())
+/// 	.and_then(handlers::create_signed_url("my_bucket_name", region, creds));
+///
+/// warp::serve(filter).run(([0.0.0.0], 3000)).await;
+/// ```
+///
+/// # Remarks
+/// This method merely leverages rusoto_s3 to generate a URL, and no
+/// external requests are made. It's not feasible that this method could
+/// error, thus declared as infallible.
 pub async fn create_signed_url(bucket_name: String, region: Region, credentials: AwsCredentials) -> Result<impl warp::Reply, Infallible> {
 	let req = PutObjectRequest {
 		bucket: bucket_name.to_owned(),
--- src/responses/mod.rs
+++ src/responses/mod.rs
@@ -1,6 +1,8 @@
 use serde::Serialize;
 
+/// A response used for transmitting pre-signed URLs.
 #[derive(Serialize)]
 pub struct PreSignedUrlResponse {
+	/// The pre-signed URL to return.
 	pub url: String,
 }
